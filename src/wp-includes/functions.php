8b797975cdde622c898814caa7e1f2d44c293699
DrewAPicture <info@drewapicture.com>
2017-08-23 21:48:38 -0600

    Remove RDF feed handling.

--- src/wp-includes/functions.php
+++ src/wp-includes/functions.php
@@ -1246,17 +1246,6 @@ function do_feed() {
 }
 
 /**
- * Load the RDF RSS 0.91 Feed template.
- *
- * @since 2.1.0
- *
- * @see load_template()
- */
-function do_feed_rdf() {
-	load_template( ABSPATH . WPINC . '/feed-rdf.php' );
-}
-
-/**
  * Load the RSS 1.0 Feed Template.
  *
  * @since 2.1.0
